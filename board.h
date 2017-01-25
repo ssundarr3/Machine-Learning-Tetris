d9f7b9ef53a25944a586a5905da04cb08ba99f41
Raymond Zheng <raymond1wen1zheng1@gmail.com>
2017-01-24 19:46:04 -0500

    Initial neural network + Clean up (ray)

--- board.h
+++ board.h
@@ -1,15 +1,11 @@
 #include <vector>
 
-<<<<<<< HEAD
 
 void updateCoefficients(int index, double x);
 
 int getCoefficient(int index);
 
 class Board {
-=======
-class Board{
->>>>>>> origin/master
 public:
 	std::vector<std::vector<bool>> grid;
 	const int width;
@@ -22,11 +18,6 @@ public:
 		// do nothing
 	//}
 	//adds a piece to the board and returns the resulting board's fitness.
-<<<<<<< HEAD
 	double addPiece(Board b, char piece, int position, int rotation);
 	void addPiece(Board * b, char piece, int position, int rotation);
-=======
-	// int addPiece(Board b, char piece, int position, char rotation);
-	// void addPiece(Board * b, char piece, int position, char rotation);
->>>>>>> origin/master
 };
